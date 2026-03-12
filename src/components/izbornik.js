61a251f03d6c9a50c76a783f36836f9b90bc3ef9
marijadergez <45633765+marijadergez@users.noreply.github.com>
2026-03-12 20:49:26 +0100

    Rad na aplikaciji završnog rada

---
+++ src/components/izbornik.js
@@ -0,0 +1,10 @@
+export default function (){
+    return
+    <Container>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#">Navbar</Navbar.Brand>
+        </Container>
+      </Navbar>
+    </Container>
+}
