dedb144484baec5e712bf8cf986843da45e856b3
marijadergez <45633765+marijadergez@users.noreply.github.com>
2026-03-13 19:35:54 +0100

    poporavak zadaće

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
+
 import './App.css'
+import { Container } from 'react-bootstrap'
+import Izbornik from 'components/Izbornik'
+import { Route, Routes } from 'react-router-dom'
+import { RouteNames } from './constants'
+import Home from './pages/Home'
+import SmjerPregled from './pages/smjerovi/SmjerPregled'
 
 function App() {
-  
+
   return (
-    <div className="App">
-      <h1>Dobrodošli u {IME_APLIKACIJE}</h1>
-      <p>Ovo je početna stranica aplikacije.</p>
-    </div>
+   <Container>
+      <Izbornik />
+      <Routes>
+        <Route path={RouteNames.HOME} element={<Home />}  />
+        <Route path={RouteNames.SMJEROVI} element={<SmjerPregled />}  />
+      </Routes>
+    <hr />
+    &copy;Edunova
+
+   </Container>
   )
 }
 
--- src/components/constants.js
+++ src/components/constants.js
@@ -1,1 +1,6 @@
 export const IME_APLIKACIJE='Fip Hr'
+
+export const RouteNames ={
+    HOME: '/',
+    SMJEROVI: '/smjerovi'
+}
