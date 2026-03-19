a7863bc1bd7077d0c2dd1977ac7e411c6ee67627
marijadergez <45633765+marijadergez@users.noreply.github.com>
2026-03-19 20:01:44 +0100

    table

--- src/pages/usluge/UslugePregled.jsx
+++ src/pages/usluge/UslugePregled.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react"
 import UslugeService from "../../services/usluge/UslugeService"
-
-
+import { Table } from "react-bootstrap"
+import { NumericFormat } from "react-number-format"
+import {FormatDatuma} from "react-number-format"
+import { GrValidate } from "react-icons/gr"
 
 
 
@@ -10,28 +12,62 @@ export default function UslugePregled() {
 
     const [usluge, setUsluge] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         ucitajUsluge()
-    },[])
+    }, [])
 
     async function ucitajUsluge() {
-        await UslugeService.get().then((odgovor)=>{
+        await UslugeService.get().then((odgovor) => {
             setUsluge(odgovor.data)
         })
-        
+
     }
 
 
     return (
         <>
-        <ol>
-        {usluge && usluge.map((usluge)=>(
-            <li>{usluge.naziv} </li>
-        )        
-        )}
+            <Table>
 
-        </ol>
+                <thead>
+                    <tr>
+                        <th>Naziv usluge</th>
+                        <th>Cijena jedan mjesec</th>
+                        <th>Cijena godišnja</th>
+                        <th>Paket više usluga</th>
+                        <th>Popust</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {usluge && usluge.map((usluge) => (
+                        <tr>
+                            <td>{usluge.naziv}</td>
+                            <td>{usluge.trajanje} h</td>
+                            <td>
+                                <NumericFormat
+                                    value={usluge.cijena}
+                                    displayType={'text'}
+                                    thousandSeparator='.'
+                                    decimalSeparator=','
+                                    suffix=' €'
+                                    prefix=' ='
+                                    decimalScale={2}
+                                    fixedDecimalScale  />
 
-
+                            </td>
+                            <td>
+                                <FormatDatuma datum={usluge.datumPokretanja} />
+                            </td>
+                            <td>
+                                <GrValidate
+                                    size={25}
+                                    color={smjer.aktivan ? 'green' : 'red'}
+                                />
+                            </td>
+                            <td></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
         </>
- )}
+    )
+}
