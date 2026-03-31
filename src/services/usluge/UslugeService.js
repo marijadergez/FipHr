82044a9f3e4e6530a1bdf7fda33e5bec38c875b6
marijadergez <45633765+marijadergez@users.noreply.github.com>
2026-03-31 20:16:06 +0200

    promjene

--- src/services/usluge/UslugeService.js
+++ src/services/usluge/UslugeService.js
@@ -1,10 +1,34 @@
+<<<<<<< Updated upstream
 import { usluge } from "./UslugePodaci";
+=======
+
+
+import { DATA_SOURCE } from "../../constants";
+import UslugeServiceLocalStorage from "./UslugeServiceLocalStorage";
+import UslugeServiceMemorija from "./UslugeServiceMemorija";
+
+
+
+let Servis = null;
+
+switch (DATA_SOURCE) {
+    case 'memorija':
+        Servis = UslugeServiceMemorija;
+        break;
+
+    case 'localStorage':
+        Servis = UslugeServiceLocalStorage;
+        break;
+    default:
+        Servis = null;
+>>>>>>> Stashed changes
 
 async function get() {
     return {data: [...usluge]}
     
 }
 
+<<<<<<< Updated upstream
 
 async function dodaj(usluga) {
     if(usluge.length===0){
@@ -43,3 +67,25 @@ export default{
     promjeni,
     obrisi
 }
+=======
+const PrazanServis = {
+    get: async () =>({ success: false, data: [] }),
+    getBySifra: async (sifra) => ({ success: false, data: {} }),
+    dodaj: async (usluga) => { console.error("Servis nije učitan"); },
+    prmjeni: async (usluga.smjer) => { console.error("Servis nije učitan"); },
+    obrisi: async (sifra) => { console.error("Servis nije dostupan"); }
+
+}
+
+
+const AktivniServis = Servis || PrazanServis;
+
+export default {
+    get: () => AktivniServis.get(),
+    getBySifra: (sifra) => AktivniServis.getBySifra(sifra),
+    dodaj: (usluga) => AktivniServis.dodaj(usluga),
+    promjeni: (sifra, usluga) => AktivniServis.promjeni(sifra),
+    obrisi: (sifra) => AktivniServis.obrisi(sifra)
+}
+
+>>>>>>> Stashed changes
